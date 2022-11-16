9ba9450497aca060ea41fa755921413a52d47828
DonShan <msdjshan47@gmail.com>
2022-11-16 14:44:13 +0530

    Initial Commit

---
+++ MyToDos/MyToDosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyToDosApp.swift
+//  MyToDos
+//
+//  Created by Madushan Senavirathna on 2022-11-16.
+//
+
+import SwiftUI
+
+@main
+struct MyToDosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
